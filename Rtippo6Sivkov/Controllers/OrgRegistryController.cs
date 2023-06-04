85fd9105bc02b90d501fdd963c9f8b3bb1c6ee5e
SavelySivkov <sivkov@mindbox.cloud>
2023-06-04 14:35:03 +0500

    Add add organization page

--- Rtippo6Sivkov/Controllers/OrgRegistryController.cs
+++ Rtippo6Sivkov/Controllers/OrgRegistryController.cs
@@ -1,3 +1,4 @@
+using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Rtippo6Sivkov.Controllers;
@@ -9,4 +10,40 @@ public class OrgRegistryController : Controller
     {
         return View();
     }
+
+    [HttpGet("Add")]
+    public IActionResult GetAdd()
+    {
+        return View("Add");
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult PostAdd([FromForm] AddOrganizationViewModel addOrganizationViewModel)
+    {
+        if (!ModelState.IsValid)
+        {
+            ModelState.AddModelError("X", "YZ");
+            return RedirectToAction("GetAdd");
+        }
+        return Json(addOrganizationViewModel);
+    }
+}
+
+public class AddOrganizationViewModel
+{
+    [Required]
+    public string Name { get; set; }
+    [Required]
+    public int Inn { get; set; }
+    [Required]
+    public int Kpp { get; set; }
+    [Required]
+    public string Address { get; set; }
+    [Required]
+    public bool IsPhysical { get; set; }
+    [Required]
+    public int TypeId { get; set; }
+    [Required]
+    public int LocalityId { get; set; }
 }
