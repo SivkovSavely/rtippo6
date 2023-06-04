9bf7b45ee8950ac10289ee128282be6018e71cb6
SavelySivkov <sivkov@mindbox.cloud>
2023-06-05 01:01:48 +0500

    Make OrgRegistryController accessible to authorized users only

--- Rtippo6Sivkov/Controllers/OrgRegistryController.cs
+++ Rtippo6Sivkov/Controllers/OrgRegistryController.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Rtippo6Sivkov.Data;
 using Rtippo6Sivkov.Models;
@@ -6,6 +7,7 @@ using Rtippo6Sivkov.Models;
 namespace Rtippo6Sivkov.Controllers;
 
 [Route("OrgRegistry")]
+[Authorize]
 public class OrgRegistryController : Controller
 {
     private readonly ApplicationDbContext _dbContext;
