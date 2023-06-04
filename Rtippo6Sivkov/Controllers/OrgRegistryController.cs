558f7fca2cb70017c36cc00ba472a68783c84c8b
SavelySivkov <sivkov@mindbox.cloud>
2023-06-05 00:19:01 +0500

    Actually create Organization in OrgRegistryController.PostAdd

--- Rtippo6Sivkov/Controllers/OrgRegistryController.cs
+++ Rtippo6Sivkov/Controllers/OrgRegistryController.cs
@@ -1,11 +1,20 @@
 using System.ComponentModel.DataAnnotations;
 using Microsoft.AspNetCore.Mvc;
+using Rtippo6Sivkov.Data;
+using Rtippo6Sivkov.Models;
 
 namespace Rtippo6Sivkov.Controllers;
 
 [Route("OrgRegistry")]
 public class OrgRegistryController : Controller
 {
+    private readonly ApplicationDbContext _dbContext;
+
+    public OrgRegistryController(ApplicationDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+
     public IActionResult Index()
     {
         return View();
@@ -26,24 +35,35 @@ public class OrgRegistryController : Controller
             ModelState.AddModelError("X", "YZ");
             return RedirectToAction("GetAdd");
         }
-        return Json(addOrganizationViewModel);
+
+        var locality = _dbContext.Localities.Single(x => x.Id == addOrganizationViewModel.LocalityId);
+        var type = _dbContext.OrganizationsTypes.Single(x => x.Id == addOrganizationViewModel.TypeId);
+
+        var org = new Organization
+        {
+            Name = addOrganizationViewModel.Name,
+            Address = addOrganizationViewModel.Address,
+            Inn = addOrganizationViewModel.Inn.ToString(),
+            Kpp = addOrganizationViewModel.Kpp.ToString(),
+            IsPhysical = addOrganizationViewModel.IsPhysical,
+            Locality = locality,
+            Type = type
+        };
+
+        _dbContext.Organizations.Add(org);
+        _dbContext.SaveChanges();
+
+        return View("Index");
     }
 }
 
 public class AddOrganizationViewModel
 {
-    [Required]
-    public string Name { get; set; }
-    [Required]
-    public int Inn { get; set; }
-    [Required]
-    public int Kpp { get; set; }
-    [Required]
-    public string Address { get; set; }
-    [Required]
-    public bool IsPhysical { get; set; }
-    [Required]
-    public int TypeId { get; set; }
-    [Required]
-    public int LocalityId { get; set; }
+    [Required] public string Name { get; set; }
+    [Required] public int Inn { get; set; }
+    [Required] public int Kpp { get; set; }
+    [Required] public string Address { get; set; }
+    [Required] public bool IsPhysical { get; set; }
+    [Required] public int TypeId { get; set; }
+    [Required] public int LocalityId { get; set; }
 }
