d60b2def3b2c88cd071bc56eac383610f94284b8
SavelySivkov <sivkov@mindbox.cloud>
2023-06-07 12:54:36 +0500

    Add Locality to AppUser

---
+++ Rtippo6Sivkov/Data/Migrations/20230607075132_AppUser_Add-Locality.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rtippo6Sivkov.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AppUser_AddLocality : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "LocalityId",
+                table: "AspNetUsers",
+                type: "INTEGER",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_LocalityId",
+                table: "AspNetUsers",
+                column: "LocalityId");
+
+            migrationBuilder.Sql("UPDATE AspNetUsers SET LocalityId=1");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Localities_LocalityId",
+                table: "AspNetUsers",
+                column: "LocalityId",
+                principalTable: "Localities",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Localities_LocalityId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_LocalityId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "LocalityId",
+                table: "AspNetUsers");
+        }
+    }
+}
--- Rtippo6Sivkov/Models/AppUser.cs
+++ Rtippo6Sivkov/Models/AppUser.cs
@@ -4,5 +4,6 @@ namespace Rtippo6Sivkov.Models;
 
 public class AppUser : IdentityUser
 {
+    public Locality Locality { get; set; } = null!;
     public AppUserRole Role { get; set; } = null!;
 }
